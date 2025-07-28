1083bdf755ad3474d8efcee9f118bfe2eb42cde4
adiletkasymbaev <kws.kgz@gmail.com>
2025-07-28 09:43:48 +0600

    feat: customize (tabs) layout to correctly show all 4 pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,15 +2,26 @@ import { Tabs } from 'expo-router'
 import React from 'react'
 
 const _Layout = () => {
+    const tabs = [
+        {title: 'Home', name: 'index'},
+        {title: 'Search', name: 'search'},
+        {title: 'Saved', name: 'saved'},
+        {title: 'Profile', name: 'profile'},
+    ]
+
   return (
     <Tabs>
-        <Tabs.Screen
-            name="index"
-            options={{
-                title: 'Home',
-                headerShown: false
-            }}
-        />
+        {tabs.map(tab => {
+            return (
+                <Tabs.Screen
+                    name={tab.name}
+                    options={{
+                        title: tab.title,
+                        headerShown: false
+                    }}
+                />
+            )
+        })}
     </Tabs>
   )
 }
