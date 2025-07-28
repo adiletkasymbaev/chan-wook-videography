761a8a3116b58cc7b1b59e80f093958aad647ac3
adiletkasymbaev <kws.kgz@gmail.com>
2025-07-28 09:32:46 +0600

    feat: turned off the native navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,20 @@ import { Stack } from "expo-router";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      {/* Turning off the ugly header on top of the screens */}
+
+      {/* Navigator for the (tabs) group */}
+      <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false }}
+      />
+      {/* Navigator for the movie detail page */}
+      <Stack.Screen
+        name="movie/[id]"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  );
 }
